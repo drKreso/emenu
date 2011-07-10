103200edf50df10f88cf0f55d6d4a625f9335c34
Kresimir Bojcic <kresimir.bojcic@gmail.com>
2011-07-10 19:28:51 +0200

    Version bump to 0.1.5

--- lib/generators/emenu/install_generator.rb
+++ lib/generators/emenu/install_generator.rb
@@ -17,6 +17,23 @@ module Emenu
         copy_file 'stylesheets/reset.css', 'public/stylesheets/reset.css'
       end
 
+      def include_emenu_and_default_menu
+        inject_into_class "app/controllers/application_controller.rb", "  include Emenu\n"
+        inject_into_class "app/controllers/application_controller.rb", ApplicationController do
+             "  def menu\n"
+             "    menu = MenuConfig.new\n"
+             "    menu.item :my_menu do\n"
+             "      item :something\n"
+             "    end\n"
+             "    return menu\n"
+             "  end\n"
+        end
+      end
+
+      def add_route
+        route "match 'emenu_state' => 'application#emenu_state'\n"
+      end
+
       def explain
         puts "Enterprise menu is successfully installed"
         puts "You need to link 'reset.css' and 'emenu.css' from layout file"
