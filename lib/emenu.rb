8875cd83c50535d640775b176ef00aea70c8108c
Kresimir Bojcic <kresimir.bojcic@gmail.com>
2011-07-10 17:59:36 +0200

    Version bump to 0.1.4

--- lib/emenu.rb
+++ lib/emenu.rb
@@ -3,3 +3,20 @@ require 'action_view'
 require 'emenu/string.rb'
 require 'emenu/menu.rb'
 require 'emenu/menu_config.rb'
+
+module Emenu
+  def emenu_state
+    session[:menu_state] ||= {}
+    session[:menu_state][params[:id]] = params[:state] unless params[:id].nil?
+    render :json => { :data => :ok } 
+  end
+
+  def emenu 
+    @menu = menu
+    session[:menu_state].each { |key, value| @menu.open(key.to_sym) if value == "opened"} unless session[:menu_state].nil?
+  end
+
+  def self.included(base)
+    base.before_filter :emenu
+  end
+end
