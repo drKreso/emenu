634276d03244c02570b63011e0e16c45775792f4
Kresimir Bojcic <kresimir.bojcic@gmail.com>
2011-07-10 10:26:51 +0200

    Version bump to 0.0.33

--- lib/generators/emenu/install_generator.rb
+++ lib/generators/emenu/install_generator.rb
@@ -3,12 +3,24 @@ require 'rails/generators'
 module Emenu
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)  
-      def generate_install
-        puts "Hello from generator"
+      def copy_images
+        directory 'images/emenu'
       end
 
-      def manifest
-        puts "Hello from manifest"
+      def copy_javscript
+        copy_file 'javascripts/jquery-1.4.2.min.js', 'public/javascripts/jquery-1.4.2.min.js'
+        copy_file 'javascripts/emenu.js', 'public/javascripts/emenu.js'
+      end
+
+      def copy_stylesheets
+        copy_file 'stylesheets/emenu.cs', 'public/stylesheets/emenu.css'
+        copy_file 'stylesheets/reset.cs', 'public/stylesheets/reset.css'
+      end
+
+      def explain
+        puts "Enterprise menu is successfully installed"
+        puts "You need to link 'reset.css' and 'emenu.css' from layout file"
+        puts "You need to link 'jquery-1.4.2.min.js' and 'emenu.js' from layot file"
       end
     end  
 end
